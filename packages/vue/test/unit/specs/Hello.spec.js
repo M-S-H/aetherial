56da81be88b5661709dece278a39e1814ee94bbb
Michael Hickman <mshickman91@gmail.com>
2018-04-12 23:53:27 -0600

    vue testing

--- packages/vue/test/unit/specs/Hello.spec.js
+++ packages/vue/test/unit/specs/Hello.spec.js
@@ -1,9 +1,9 @@
-// import Vue from 'vue'
+// // import Vue from 'vue'
 // import Hello from '@/components/Hello'
 
-// describe('Hello.vue', () => {
-//   it('should render correct contents', () => {
-//     expect(true)
-//       .to.equal(true)
-//   })
-// })
+describe('Hello.vue', () => {
+  it('should render correct contents', () => {
+    expect(true)
+      .to.equal(true)
+  })
+})
