1d830e817e32cccb7a15e5074d84ed3b0160c3a0
Michael Hickman <mshickman91@gmail.com>
2020-09-30 11:40:34 -0600

    added comps

---
+++ packages/vue/src/components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    class="av-button"
+    :disabled="disabled"
+    v-av-ripple
+    @click="propagateClick"
+  >
+    <!-- Main Button Text -->
+    <span class="av-button-content">
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    state: {
+      type: String,
+      default: 'button'
+    }
+  },
+
+  methods: {
+    propagateClick ($event: MouseEvent) {
+      $event.stopImmediatePropagation()
+      this.$emit('click')
+    }
+  }
+})
+</script>
